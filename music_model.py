919aca668d96af7784cc54f1f244c8910afec94a
sarvagyapatel <103870100+sarvagyapatel@users.noreply.github.com>
2024-08-13 15:07:51 +0530

    Update music_model.py

--- music_model.py
+++ music_model.py
@@ -70,4 +70,5 @@ def predict():
         return jsonify({'predicted_genre': genre_dict[predicted_index]})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
